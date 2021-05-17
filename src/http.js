a470d3aba46f70e003f2285d361a8b6cd45bf4a7
nicholasvuono <nicholas.vuono@gmail.com>
2021-05-16 22:32:45 -0400

    added batch http functionality without worker parallelism to simulate virtual users

--- src/http.js
+++ src/http.js
@@ -1,7 +1,30 @@
+const http = require("http");
+const async = require("async");
+
 class Http {
+  constructor() {
+    this._requests = [];
+    this._results = [];
+  }
+
   options(options) {
     this._options = options;
     return this;
+  }
+
+  batch(requests) {
+    for (var i = 0; i < requests.length; i++) {
+      this._requests.push(function (callback) {
+        http
+          .request(requests[i], function (res) {
+            callback(null, res.statusCode);
+          })
+          .end();
+      });
+    }
+    async.parallelLimit(requests, this._options.limit, function (err, results) {
+      this._results.push(JSON.stringify(results));
+    });
   }
 }
 
