4cc8f7d734118541c2234c1f9851943bfedd1498
nicholasvuono <nicholas.vuono@gmail.com>
2021-05-30 11:02:18 -0400

    exported Http in index file, upgraded package version to 0.3.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Browser } = require("./src/browser.js");
 const { Steps } = require("./src/steps.js");
+const { Http } = require("./src/http.js");
 
 exports.Browser = Browser;
 exports.Steps = Steps;
+exports.Http = Http;
